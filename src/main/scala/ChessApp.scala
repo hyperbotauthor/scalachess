7d3feac192930cf8ef8a155a8e74d5be8ebe1bf2
hyperbotauthor <hyperchessbotauthor@gmail.com>
2020-10-28 15:02:37 +0000

    Export a function

--- src/main/scala/ChessApp.scala
+++ src/main/scala/ChessApp.scala
@@ -1,7 +1,13 @@
 package chess
 
+import scala.scalajs.js.annotation.JSExportTopLevel
+
 object ChessApp {
-  def main(args: Array[String]): Unit = {
-    println("Hello world!")
-  }
+	@JSExportTopLevel("exportFunc")
+	def exportFunc(): Unit = {
+		println("exportFunc!")
+	}
+	def main(args: Array[String]): Unit = {
+		println("Hello world!")		
+	}
 }
