9db40d2cbe47baf42af9947468d931ec4a1a74d8
hyperbotauthor <hyperchessbotauthor@gmail.com>
2020-10-28 18:11:37 +0000

    Construct game

--- src/main/scala/ChessApp.scala
+++ src/main/scala/ChessApp.scala
@@ -5,8 +5,15 @@ package chess
 object ChessApp {
 	//@JSExportTopLevel("exportFunc")
 	def exportFunc(): Unit = {
-		val p = Piece(White, King)
-		println(p)
+		val p1 = Piece(White, King)
+		val p2 = Piece(Black, King)
+		val pos1 = Pos.A1
+		val pos2 = Pos.E4
+		val pm : PieceMap = Map(pos1 -> p1, pos2 -> p2)		
+		val b = Board(pm, History(), variant.Atomic, None)
+		val s = Situation(b, White)
+		val g = Game(s)
+		println(g)
 	}
 	def main(args: Array[String]): Unit = {
 		println("Hello world!")		
