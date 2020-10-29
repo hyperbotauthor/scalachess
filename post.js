9d1a5a57ef452969f3e3850b79838194915c262d
hyperbotauthor <hyperchessbotauthor@gmail.com>
2020-10-29 15:00:55 +0000

    Game pgn

--- post.js
+++ post.js
@@ -16,6 +16,122 @@ function makeUciMoves(variantKey, fen, uciMoves){
 	}
 }
 
-let result = makeUciMoves("atomic", undefined, ["e2e3", "e7e6", "g1f3"])
+class GameNode_{
+	constructor(parentGame, props){
+		this.parentGame = parentGame
+		this.id = props.id || "root"
+		this.parentId = props.parentId
+		this.fen = props.fen
+		this.uci = props.uci
+		this.san = props.san
+		this.childIds = props.childIds || []
+	}
+	
+	getNodeById(id){
+		return this.parentGame.getNodeById(id)
+	}
+	
+	makeUciMove(uci, fen, san){
+		let newId = this.id + "_" + uci
+		let node
+		if(this.childIds.includes(newId)){
+			let node = this.getNodeById(newId)
+		}else{
+			this.childIds.push(newId)
+			node = GameNode(this.parentGame, {id: newId, fen: fen, uci: uci, san: san})			
+			node.parentId = this.id
+		}		
+		this.parentGame.addNode(node)
+		this.parentGame.currentNode = node
+	}
+	
+	fullmoveNumber(){
+		return parseInt(this.fen.split(" ")[5])
+	}
+	
+	turn(){
+		return this.fen.split(" ")[1]
+	}
+	
+	getParent(){
+		if(!this.parentId) return null
+		return this.parentGame.getNodeById(this.parentId)
+	}
+	
+	pgn(){		
+		let parent = this.getParent()
+		if(!parent) return ""
+		let number = ""		
+		if(!parent.parentId) number = `${(this.fullmoveNumber() - 1)}..`		
+		if(this.turn() == "b") number = `${this.fullmoveNumber()}.`				
+		if(number) return number + " " + this.san
+		return this.san
+	}
+}
+function GameNode(parentGame, props){return new GameNode_(parentGame, props)}
 
-console.log(result)
+class Game_{
+	constructor(variantKey, fen){
+		this.variantKey = variantKey
+		let result = makeUciMoves(this.variantKey, fen)
+		this.currentNode = GameNode(this, {fen: result.fen})
+		this.nodes = {root: this.currentNode}
+	}
+	
+	makeUciMove(uci){
+		let result = makeUciMoves(this.variantKey, this.currentNode.fen, [uci])
+		
+		if(result.success){			
+			this.currentNode.makeUciMove(uci, result.fen, result.sanMoves[0])
+			return true
+		}else return false
+	}
+	
+	getNodeById(id){
+		return this.nodes[id]
+	}
+	
+	addNode(node){
+		this.nodes[node.id] = node
+	}
+	
+	line(){
+		let cn = this.currentNode
+		let nodes = []
+		while(cn && cn.parentId){
+			nodes.unshift(cn)			
+			cn = this.getNodeById(cn.parentId)
+		}
+		return nodes
+	}
+	
+	lineSan(){
+		return this.line().map(node => node.san).join(" ")
+	}
+	
+	linePgn(){
+		return this.line().map(node => node.pgn()).join(" ")
+	}
+	
+	fen(){
+		return this.currentNode.fen
+	}
+}
+function Game(variantKey, fen){return new Game_(variantKey, fen)}
+
+function newGame(variantKey, fen){
+	let result = makeUciMoves(variantKey, fen, [])
+	if(result.success) return Game(variantKey, fen)
+	return null
+}
+
+let game = newGame("atomic")
+
+game.makeUciMove("e2e4")
+
+game = newGame("atomic", game.fen())
+
+game.makeUciMove("d7d5")
+game.makeUciMove("g1f3")
+
+console.log(`${game.linePgn()}`)
