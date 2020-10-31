9602ac9500a90129963fdf0fe50fd8c159a88179
hyperbotauthor <hyperchessbotauthor@gmail.com>
2020-10-31 19:04:50 +0000

    Heroku

---
+++ web/server.js
@@ -0,0 +1,15 @@
+
+const path = require('path')
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 3000
+
+app.get('/', (req, res) => {
+	res.sendFile(path.join(__dirname, "index.html"))
+})
+
+app.use('/', express.static(__dirname))
+
+app.listen(port, _ => {
+    console.log(`Hypereasy listening on port ${port} !`)
+})
