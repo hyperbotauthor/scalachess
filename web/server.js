7ba566ac0f4ba9e541091669a84e34fe1b5a0a0b
hyperbotauthor <hyperchessbotauthor@gmail.com>
2020-10-31 23:20:10 +0000

    Log only cookies

--- web/server.js
+++ web/server.js
@@ -6,7 +6,7 @@ const app = express()
 const port = process.env.PORT || 3000
 
 app.get('/', (req, res) => {
-	if(req.query.logreq) console.log("cookies", req.cookies, "cookieToUser", oauth.cookieToUser)
+	if(req.query.logreq) console.log("cookies", req.cookies)
 	res.send(`user: _${req.user}_<br>
 <a href="/auth/lichess/bot">login</a>
 `)
